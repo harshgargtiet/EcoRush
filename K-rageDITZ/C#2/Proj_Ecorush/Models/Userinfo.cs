099ec3c068ebdf6b56f28193e497f35c826f5d36
K-Rage <sbhadauria_be19@thapar.edu>
2023-09-11 19:51:46 +0530

    Added the scaffolded C#

---
+++ K-rageDITZ/C#2/Proj_Ecorush/Models/Userinfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Proj_Ecorush.Models;
+
+public partial class Userinfo
+{
+    public string EmailId { get; set; } = null!;
+
+    public string Username { get; set; } = null!;
+
+    public string? Password { get; set; }
+
+    public string Officelocation { get; set; } = null!;
+
+    public int Ccpoints { get; set; }
+
+    public string Urole { get; set; } = null!;
+
+    public virtual ICollection<Afforestation> Afforestations { get; set; } = new List<Afforestation>();
+
+    public virtual ICollection<Carpooling> Carpoolings { get; set; } = new List<Carpooling>();
+
+    public virtual ICollection<EvTravel> EvTravels { get; set; } = new List<EvTravel>();
+
+    public virtual ICollection<WalkCycle> WalkCycles { get; set; } = new List<WalkCycle>();
+}
