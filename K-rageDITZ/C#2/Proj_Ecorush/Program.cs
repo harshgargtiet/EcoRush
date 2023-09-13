277bf06477cf419dccacaf541bd265b04a510810
K-Rage <sbhadauria_be19@thapar.edu>
2023-09-13 08:16:50 +0000

    Added walk_cycle table apis

--- K-rageDITZ/C#2/Proj_Ecorush/Program.cs
+++ K-rageDITZ/C#2/Proj_Ecorush/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Proj_Ecorush.Models;
+using Proj_Ecorush.Services.Interfaces;
+using Proj_Ecorush.Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<EcoRushDbContext>
+(optionsAction: options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnectionString")));
+builder.Services.AddScoped<IWalkingCycle, WalkCycleServices>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ K-rageDITZ/C#2/Proj_Ecorush/Services/Services/WalkCycleServices.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proj_Ecorush.Models;
+using Proj_Ecorush.Services.Interfaces;
+
+namespace Proj_Ecorush.Services.Services
+{
+    public class WalkCycleServices : IWalkingCycle
+    {
+        public EcoRushDbContext? _context;
+
+        public WalkCycleServices(EcoRushDbContext? context)
+        {
+            _context = context;
+        }
+        public async Task<string> AddNewActivityWC(WalkCycle walkCycle)
+        {
+            _context.WalkCycles.Add(walkCycle);
+            await _context.SaveChangesAsync();
+
+            return "Activity successfully saved";
+        }
+
+        public async Task<List<WalkCycle>> GetWCbyStatus(string status)
+        {
+            List<WalkCycle> walkCycles = await _context.WalkCycles.ToListAsync();
+            walkCycles = walkCycles.Where(wc => wc.Status == status).ToList();
+
+            if (walkCycles == null)
+            {
+                throw new Exception();
+            }
+
+            return walkCycles;
+        }
+
+        public async Task<List<WalkCycle>> GetAllActivitiesWC()
+        {
+            
+            List<WalkCycle> walkCycles = await _context.WalkCycles.ToListAsync();
+            return walkCycles;
+        }
+
+        public async Task<List<WalkCycle>> GetWCbyEmail(string email)
+        {
+            List<WalkCycle> walkCycles = await _context.WalkCycles.ToListAsync();
+            walkCycles = walkCycles.Where(wc=>wc.EmailId==email).ToList();
+
+            if (walkCycles == null)
+            {
+                throw new Exception();
+            }
+
+            return walkCycles;
+        }
+
+        public async Task<string> UpdateActivityWC(int activityID, WalkCycle walkCycle)
+        {
+            var walkCycles = await _context.WalkCycles.FindAsync(activityID);
+
+
+
+            if (walkCycles == null)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                walkCycles.Evidence = walkCycle.Evidence;
+                walkCycles.Distance = walkCycle.Distance;
+                walkCycles.ActivityDate = walkCycle.ActivityDate;
+                walkCycles.Ccawarded = walkCycle.Ccawarded;
+                walkCycles.Status = walkCycle.Status;
+                await _context.SaveChangesAsync();
+
+                return "Update was successfull";
+            }
+        }
+    }
+}
